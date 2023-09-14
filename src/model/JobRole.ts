8473a46f018889d1efebbe95a9afa7a8d62f805b
Callum <callum.esprey@kainos.com>
2023-09-14 09:37:03 +0100

    Created the JobRole.ts Model

---
+++ src/model/JobRole.ts
@@ -0,0 +1,9 @@
+export class JobRole{
+    RoleID: number
+    Name: string
+    Job_Spec: string
+    Responsibilities: string
+    Sharepoint_Link: string
+    BandID: number
+    FamilyID: number
+}
