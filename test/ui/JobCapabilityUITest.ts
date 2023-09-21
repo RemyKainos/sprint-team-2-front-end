b107b8b13e92d8f8267cb9b52f4955c28f369406
sammyperry101 <sammy2580@hotmail.com>
2023-09-21 14:03:48 +0100

    added ui tests

---
+++ test/ui/JobCapabilityUITest.ts
@@ -0,0 +1,47 @@
+import { Builder, Capabilities, By } from 'selenium-webdriver';
+
+import chai from 'chai';  
+
+describe('Job Capability UI Test', async () => {
+    it('should select valid capability and redirect to family by capability page', async () => {
+        const driver = new Builder().
+            withCapabilities(Capabilities.chrome()).
+            build();
+
+        await driver.get(process.env.FRONT_URL + '/select-capability' as string)
+
+        const dropdown = await driver.findElement(By.id('capabilityID'))
+
+        await dropdown.click()
+
+        const optionText = 'Platforms'
+        const option = await driver.findElement(By.xpath(`//option[text()='${optionText}']`));
+        await option.click()
+
+        await driver.findElement(By.id('submit')).click();
+
+        await driver.findElement(By.id('title')).getText().then(function(value:string) {
+            console.log(value)
+            chai.assert.equal(value, 'Families for Platforms Capability')
+        })
+
+        await driver.quit()
+    })
+
+    it('should display families not got page when id is not correct', async () => {
+        const driver = new Builder().
+            withCapabilities(Capabilities.chrome()).
+            build();
+
+        const invalidID = 10000
+
+        await driver.get(process.env.FRONT_URL + '/family-by-capability/' + invalidID.toString() as string)
+
+        await driver.findElement(By.id('title')).getText().then(function(value:string) {
+            console.log(value)
+            chai.assert.equal(value, 'Families not got')
+        })
+
+        await driver.quit()
+    })
+})
