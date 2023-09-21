4e73ccb36544f576c8db829f6df164b46c282e14
sammyperry101 <sammy2580@hotmail.com>
2023-09-21 10:51:17 +0100

    added JobFamilyControllerTests

--- test/unit/jobFamilyControllerTest.ts
+++ test/unit/jobFamilyControllerTest.ts
@@ -1,23 +1,65 @@
 import { expect } from 'chai';
 import { Request, Response } from 'express';
+import sinon from 'sinon';
 import { JobFamilyController } from '../../src/controller/JobFamilyController';
+import * as jobCapabilityService from '../../src/service/jobCapabilityService';
+import * as jobFamilyService from '../../src/service/jobFamilyService';
+import { JobCapability } from '../../src/model/JobCapability';
+import { JobFamily } from '../../src/model/JobFamily';
 
 describe('JobFamilyController', () => {
-    describe('get', () => {
-        it('should correctly render the Family by Capability page', () => {
-            const req: Request = {
-                body: {
-                    capabilityID: 1,
-                },
-                session: {},
-            } as Request;
-            const res = {
-                render: (viewName: string) => {
-                    expect(viewName).to.equal('family-by-capability');
-                },
-            } as Response;
+    afterEach(() => {
+      sinon.restore();
+    });
 
-            JobFamilyController.get(req, res);
-        })
-    })
+    it("should render the family-by-capability view with data", async () => {
+        const req: Partial<Request> = {
+        params: { id: "1" },
+        } as Partial<Request>;
+
+        const res = {
+            render: sinon.spy()
+        } 
+
+        const mockFamilies: JobFamily[] = [{
+            capabilityID: 1,
+            familyID: 1,
+            name: "name"
+        }]
+        const mockCapability: JobCapability =  {
+            capabilityID: 1,
+            name: "name"
+        }
+
+        const getFamilyByCapabilityStub = sinon.stub(jobFamilyService, "getFamilyByCapability");
+        const getCapabilityByIdStub = sinon.stub(jobCapabilityService, "getCapabilityById");
+
+        getFamilyByCapabilityStub.withArgs(1).resolves(mockFamilies);
+        getCapabilityByIdStub.withArgs(1).resolves(mockCapability);
+
+        await JobFamilyController.get(req as Request, res as unknown as Response);
+
+        expect(getFamilyByCapabilityStub.calledOnceWithExactly(1)).to.be.true;
+        expect(getCapabilityByIdStub.calledOnceWithExactly(1)).to.be.true;
+        expect(res.render.calledOnceWithExactly("family-by-capability", { families: mockFamilies, capability: mockCapability })).to.be.true;
+    });
+
+    it("should handle errors and log them", async () => {
+        const req: Partial<Request> = {
+          params: { id: "1" },
+        } as Partial<Request>;
+    
+        const res= {
+            render: sinon.spy()
+        }
+    
+        const getFamilyByCapabilityStub = sinon.stub(jobFamilyService, "getFamilyByCapability").rejects("Could not fetch family by capability");
+    
+        const consoleErrorStub = sinon.stub(console, "error");
+    
+        await JobFamilyController.get(req as Request, res as unknown as Response);
+    
+        expect(getFamilyByCapabilityStub.calledOnceWithExactly(1)).to.be.true;
+        expect(consoleErrorStub.calledOnce).to.be.true;
+      });
 })
