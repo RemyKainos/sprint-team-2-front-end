36b83d1692a00f9e06d701dd42d6a19981e62fdd
Mateen Parkar <mateenparkar4@gmail.com>
2023-09-22 15:47:04 +0100

    Changes made to front end and comments addressed

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,4 +1,4 @@
-import type { ActiveSession, Credentials, User } from "../model/auth";
+import type { ActiveSession, Credentials, Role, User } from "../model/auth";
 
 import axios from 'axios';
 import type { AxiosError } from 'axios';
@@ -33,6 +33,16 @@ export const register = async function(user: User): Promise<void> {
 
     try {
         await axios.post(process.env.BACK_URL + '/api/register/', user);
+    } catch (e) {
+        throw new Error('Failed to register');
+    }
+}
+
+export const getRoles = async function():Promise<Role[]>{
+    try {
+        const response = await axios.get(process.env.BACK_URL + '/api/authRoles/');
+        console.log(response.data);
+        return response.data as Role[]
     } catch (e) {
         throw new Error('Failed to register');
     }
