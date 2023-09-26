47f4f07910d286e994b3a00e1964bec2ab13ca54
Callum <callum.esprey@kainos.com>
2023-09-26 15:18:38 +0100

    now maintains selected options

--- src/JobRoleController.ts
+++ src/JobRoleController.ts
@@ -4,12 +4,17 @@ import { JobRoleFilter } from "./model/JobRole";
 import { getAllCapabilities } from "./service/jobCapabilityService";
 
 export class JobRoleController {
+    
+
 
     public static get = async function(req:Request, res:Response): Promise<void> {
+        const bands = 
+        ["Leadership Community", "Principal", "Manager", "Consultant", "Senior Associate", "Associate", "Trainee", "Apprentice"]
+        
         try{
             const roles = await viewJobRoles();
             const capabilities = await getAllCapabilities()
-            res.render('ViewRoles.html', {title: "View Roles", roles: roles, capabilities: capabilities})
+            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities})
         } catch(e){
             console.error(e);
             res.render('ViewRoles.html', {title: "View Roles Error", errorMessage: e as string})
@@ -17,6 +22,8 @@ export class JobRoleController {
     }
 
     public static post = async function(req: Request, res: Response): Promise<void> {
+        const bands = 
+        ["Leadership Community", "Principal", "Manager", "Consultant", "Senior Associate", "Associate", "Trainee", "Apprentice"]
         try{
             
             const data: JobRoleFilter = {
@@ -25,10 +32,12 @@ export class JobRoleController {
                 capabilityNameFilter: req.body.capabilityNameFilter
             }
 
+            console.log(data);
+
             const roles = await viewJobRoleWithFilter(data);
 
             const capabilities = await getAllCapabilities()
-            res.render('ViewRoles.html', {title: "View Roles", roles: roles, capabilities: capabilities})
+            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities, filters: data})
         } catch(e){
             console.error(e)
             res.render('ViewRoles.html', {title: "View Roles Error", errorMessage: e as string})
