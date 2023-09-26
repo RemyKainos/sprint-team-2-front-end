e12aa214f52da99b1d37d867cb7b1d4734dad4c7
Mateen Parkar <mateenparkar4@gmail.com>
2023-09-26 11:56:16 +0100

    Tests added for whoami

--- test/unit/service/authServiceTest.ts
+++ test/unit/service/authServiceTest.ts
@@ -5,7 +5,7 @@ import chai from 'chai';
 
 const expect = chai.expect;
 
-import { login, register } from '../../../src/service/authService'
+import { login, register, whoami } from '../../../src/service/authService'
 
 describe('AuthService', function () {
     describe('login', function () {
@@ -105,5 +105,40 @@ describe('AuthService', function () {
                 expect((error as AxiosError).message).to.equal('Password must contain at least one special character (@#$%^&+=).');
             }
         });
+        describe('whoami', function () {
+            it('should identify user successfully', async () => {
+                const mock = new MockAdapter(axios);
+                const token = 'token';
+                const responseData = { userID: 1, email: 'test@test.com', role: { roleID: 1, roleName: 'Admin' } };
+                mock.onGet(process.env.BACK_URL + '/api/whoami', { headers: { Authorization: `Bearer ${token}` } }).reply(200, responseData);
+
+                const result = await whoami(token);
+                expect(result).to.deep.equal(responseData);
+            });
+
+            it('should handle error when user is not logged in', async () => {
+                const mock = new MockAdapter(axios);
+                const token = 'token';
+                mock.onGet(process.env.BACK_URL + '/api/whoami', { headers: { Authorization: `Bearer ${token}` } }).reply(401);
+          
+                try {
+                    await whoami(token);
+                } catch (error) {
+                    expect((error as AxiosError).message).to.equal("User isn't logged in");
+                }
+            });
+
+            it('should handle error when server error', async () => {
+                const mock = new MockAdapter(axios);
+                const token = 'token';
+                mock.onGet(process.env.API_URL + '/api/whoami', { headers: { Authorization: `Bearer ${token}` } }).reply(500);
+          
+                try {
+                    await whoami(token);
+                } catch (error) {
+                    expect((error as AxiosError).message).to.equal("Couldn't get user");
+                }
+            });
+        })
     });
 });
