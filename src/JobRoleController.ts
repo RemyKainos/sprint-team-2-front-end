8005468cc0d43ea4b20978ed95d7fbe5b08ef8f0
Callum <callum.esprey@kainos.com>
2023-09-27 10:05:44 +0100

    reset now working

--- src/JobRoleController.ts
+++ src/JobRoleController.ts
@@ -8,13 +8,21 @@ export class JobRoleController {
 
 
     public static get = async function(req:Request, res:Response): Promise<void> {
+        console.log("dabbay")
+        
         const bands = 
         ["Leadership Community", "Principal", "Manager", "Consultant", "Senior Associate", "Associate", "Trainee", "Apprentice"]
+
+        const filters: JobRoleFilter = {
+            roleNameFilter: "",
+            bandNameFilter: "",
+            capabilityNameFilter: ""
+        }
         
         try{
             const roles = await viewJobRoles();
             const capabilities = await getAllCapabilities()
-            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities})
+            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities, filters: filters})
         } catch(e){
             console.error(e);
             res.render('ViewRoles.html', {title: "View Roles Error", errorMessage: e as string})
@@ -26,16 +34,27 @@ export class JobRoleController {
         ["Leadership Community", "Principal", "Manager", "Consultant", "Senior Associate", "Associate", "Trainee", "Apprentice"]
         
         const capabilities = await getAllCapabilities()
-        
-        const data: JobRoleFilter = {
-            roleNameFilter: req.body.roleNameFilter,
-            bandNameFilter: req.body.bandNameFilter,
-            capabilityNameFilter: req.body.capabilityNameFilter
+
+        console.log(req.body)
+        console.log(req.body.button)
+
+        let filters: JobRoleFilter = {
+            roleNameFilter: "",
+            bandNameFilter: "",
+            capabilityNameFilter: ""
+        }
+
+        if(req.body.button === 'filterButton'){
+            filters = {
+                roleNameFilter: req.body.roleNameFilter,
+                bandNameFilter: req.body.bandNameFilter,
+                capabilityNameFilter: req.body.capabilityNameFilter
+            }
         }
 
         try{
-            const roles = await viewJobRoleWithFilter(data);
-            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities, filters: data})
+            const roles = await viewJobRoleWithFilter(filters);
+            res.render('ViewRoles.html', {title: "View Roles", roles: roles, bands: bands, capabilities: capabilities, filters: filters})
         } catch(e){
             console.log(e)
             res.render('ViewRoles.html', {title: "View Roles Error", errorMessage: e as string})
